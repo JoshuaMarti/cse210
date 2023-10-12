d0a60a4a50ce094061b73152c4d0b8f089de3958
JoshuaMarti <joshuamartinez.personal@gmail.com>
2023-10-11 23:28:54 -0600

    Completed Week 5 Prep / Learning 03 Assignment

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+        Fraction first = new Fraction();
+        Fraction second = new Fraction(6);
+        Fraction third = new Fraction(6, 7);
+        Console.WriteLine($"First fraction: {first.GetFractionString()}");
+        Console.WriteLine($"Second fraction: {second.GetFractionString()}");
+        Console.WriteLine($"Third fraction: {third.GetFractionString()}");
+        Console.WriteLine($"Or a decimal: {third.GetDecimalValue()}");
     }
 }
